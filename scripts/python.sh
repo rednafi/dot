2ed0c8f8360e0bc2ebb20041e6606fdddc946f64
rednafi <rednafi@pop-os.localdomain>
2020-07-25 02:22:20 +0600

    Added python download script

---
+++ scripts/python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# update the packages list and install the prerequisites
+sudo apt update
+sudo apt install software-properties-common
+
+# add deadsnakes ppa to your sources' list (When prompted press Enter to continue)
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+# install python3.8
+sudo apt install python3.8
+
+# verify python installation
+python3.8 --version
