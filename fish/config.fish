ed5c6377fd6a6f70758a14cbf773d1a34b96aea9
rednafi <redowan.nafi@gmail.com>
2020-09-12 22:07:30 +0600

    Added django run command alias

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,15 @@ set -x PATH $PATH /snap/bin
 # git
 set -x GIT_EDITOR "code --wait"
 
+# alias
+alias djrs="source .venv/bin/activate.fish && python manage.py runserver"
+
+alias djq1="source .venv/bin/activate.fish &&\
+celery -A main worker -l info -Q default"
+
+alias djq2="source .venv/bin/activate.fish &&\
+celery -A main worker -Q identifiers --concurrency=1 -l info"
+
 # # >>> conda initialize >>>
 # # !! Contents within this block are managed by 'conda init' !!
 # eval /home/rednafi/miniconda3/bin/conda "shell.fish" "hook" $argv | source
