55025e15d5a2ec7f4a44174ea87974dd6c3c8f25
rednafi <redowan.nafi@gmail.com>
2020-09-26 02:14:34 +0600

    Updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -5,13 +5,13 @@ funcsave fish_greeting
 set -x PATH $PATH /snap/bin
 
 # git
-set -x GIT_EDITOR "code --wait"
+set -x GIT_EDITOR 'code --wait'
 
 # alias
-alias djrs="source .venv/bin/activate.fish && python manage.py runserver"
+alias dj_dendi="curl -s https://raw.githubusercontent.com/rednafi/dotfiles/master/scripts/dj_dendi.sh | bash"
 
-alias djq1="source .venv/bin/activate.fish &&\
-celery -A main worker -l info -Q default"
-
-alias djq2="source .venv/bin/activate.fish &&\
-celery -A main worker -Q identifiers --concurrency=1 -l info"
+# # >>> conda initialize >>>
+# # !! Contents within this block are managed by 'conda init' !!
+# eval /home/rednafi/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# conda config --set auto_activate_base false
+# # <<< conda initialize <<<
