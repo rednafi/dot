815231dca86ccc1c0ced88b90bab7c6ee3e2ed5d
Redowan Delowar <redowan.nafi@gmail.com>
2020-12-01 22:35:16 +0600

    Added pidof and pkill aliases

--- fish/config.fish
+++ fish/config.fish
@@ -4,14 +4,22 @@ funcsave fish_greeting
 # add snap path
 set -x PATH $PATH /snap/bin
 
+# add go path
+set -x PATH $PATH:/usr/local/go/bin
+
 # git
 set -x GIT_EDITOR 'code --wait'
 
 # alias
 alias dj_dendi="curl -s https://raw.githubusercontent.com/rednafi/dotfiles/master/scripts/dj_dendi.sh | bash"
 
+alias pkill="pkill -ecfi"
+alias pidof="pidof -xz"
+
+
 # # >>> conda initialize >>>
 # # !! Contents within this block are managed by 'conda init' !!
 # eval /home/rednafi/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # conda config --set auto_activate_base false
 # # <<< conda initialize <<<
+
