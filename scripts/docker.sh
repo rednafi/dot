0a65481f18eb2e3637c38d565719cef395abc77a
Redowan Delowar <redowan.nafi@gmail.com>
2021-01-19 01:22:25 +0600

    Updated docker script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# uninstall old version
+# Added bash strict mode.
+
+# Uninstall old version.
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
-# update apt
+# Update apt.
 sudo apt-get update
 
-# install packages to allow apt to use a repository over HTTPS
+# Install packages to allow apt to use a repository over HTTPS.
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -14,33 +16,33 @@ sudo apt-get install \
     gnupg-agent \
     software-properties-common
 
-# add Docker’s official GPG key
+# Add Docker’s official GPG key.
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# verify fingerprint
+# Verify fingerprint.
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-# update apt
+# Update apt.
 sudo apt-get update
 
-# install docker ce
+# Install docker ce.
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-# run hello world
+# Run hello world.
 sudo docker run hello-world
 
-# install docker-compose
+# Install docker-compose.
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
 
-# post installation (running docker as non-root)
+# Post installation (running docker as non-root).
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
