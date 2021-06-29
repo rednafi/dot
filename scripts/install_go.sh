180d5e392cf5897a8849ad266b39a2b7af4403d7
rednafi <redowan.nafi@gmail.com>
2021-06-30 05:45:16 +0600

    Added prompt to install and update go

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -3,7 +3,15 @@
 # Bash strict mode
 set -euo pipefail
 
-GO_BINARY="go1.16.4.linux-amd64.tar.gz"
+echo "Add the go version that you want to download."
+echo "You can check it here: https://golang.org/dl/"
+
+read go_version
+
+
+GO_BINARY=$(echo go{go_version}.linux-amd64.tar.gz | sed s/{go_version}/$go_version/g)
+
+echo $GO_BINARY
 
 curl -OJL https://golang.org/dl/$GO_BINARY
 
