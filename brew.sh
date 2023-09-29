a99970ab0888164aa9c9a7f998c08b7fa58138a7
redowan <redowan.nafi@gmail.com>
2023-09-29 18:41:07 -0400

    Add watch

--- brew.sh
+++ brew.sh
@@ -22,6 +22,7 @@ FORMULAE=(
     shellcheck
     stow
     tree
+    watch
     zsh
 )
 
--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 : '
-    Run all the scripts.
+    Run all the scripts. Idempotent; safe to run multiple times.
 '
 
 source ./lib.sh
